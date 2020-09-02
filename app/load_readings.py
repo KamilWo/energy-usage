14e04bdba2306949383ff109ce56d15106078c41
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-09-02 16:19:53 +0100

    Moved and improved get_readings

---
+++ app/load_readings.py
@@ -0,0 +1,27 @@
+import json
+import logging
+import os.path
+
+
+def get_readings():
+    """ Loads readings from local file system.
+
+    There's no need to refactor this function.
+    The JSON library reads until EOF and then loads into memory.
+    Returns a dictionary.
+
+    :returns: Current readings.
+    :rtype: dict
+    """
+
+    file_path = os.path.join('data', 'readings.json')
+    try:
+
+        with open(file_path, 'r') as f:
+            readings = json.load(f)
+    except FileNotFoundError as fe:
+        logging.error(f'Error: {file_path} was not found.')
+    except IOError:
+        logging.error(f'Error: Could not read {file_path}.')
+
+    return readings
